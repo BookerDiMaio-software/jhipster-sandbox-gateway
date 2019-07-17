e2537054e0c4c661fe086ced40eb247ba79754f1
camavelis <cmavelis@gmail.com>
2019-07-17 09:52:28 -0400

    Initial application generated by JHipster-6.1.2

---
+++ src/main/java/com/bookerdimaio/sandbox/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.bookerdimaio.sandbox.repository;
+
+import com.bookerdimaio.sandbox.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/bookerdimaio/sandbox/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.bookerdimaio.sandbox.service.dto;
